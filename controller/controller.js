c6c67d780dfd0843c2443ffbe9410957f6fe48d2
HAFIZI <haziq_yusof@hotmail.com>
2018-11-15 18:33:21 -0500

    Fix bugs

--- controller/controller.js
+++ controller/controller.js
@@ -3,14 +3,14 @@ let dijkstra = require('./djikstra')
 
 exports.getPath = (req,res,next)=>{
 
-    var from = dijkstra.getNodeName(req.body.from)
     var to = dijkstra.getNodeName(req.body.to)
 
-    var result = dijkstra.getPath(from, to)
+    var result = dijkstra.getPath(req.body.from, to)
+
     if (result != null){
         res.status(200).json({
-            Path : dijkstra.getPath(from, to).path,
-            TotalCost : dijkstra.getPath(from, to).cost
+            Path : result.path,
+            TotalCost : result.cost
         })
     }else{
         res.status(200).json({
@@ -18,7 +18,6 @@ exports.getPath = (req,res,next)=>{
         })
     }
 
-    // res.end()
 
 }
 
