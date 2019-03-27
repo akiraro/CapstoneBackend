c13818d7a8b40ff890b56f33a8e48114115ba91e
HAFIZI <haziq_yusof@hotmail.com>
2019-03-26 21:31:29 -0400

    fix bugs

--- controller/controller.js
+++ controller/controller.js
@@ -1,41 +1,63 @@
-const Graph = require('node-dijkstra');
-let dijkstra = require('./djikstra');
-const mapNordstorm = require('../directory/map-nordstorm');
-const directionController = require('../controller/directionController');
+const Graph = require( 'node-dijkstra' );
+let dijkstra = require( './djikstra' );
+const mapNordstorm = require( '../directory/map-nordstorm' );
+const directionController = require( '../controller/directionController' );
 
-exports.getPath = (req, res, next) => {
-    var from = mapNordstorm.decodeName(req.body.from)
-    var to = mapNordstorm.getNodeName(req.body.to)
-    var bearing = req.body.bearing
-    var result = mapNordstorm.getPath(from, to)
+exports.getPath = ( req, res, next ) => {
+    var from = mapNordstorm.decodeName( req.body.from )
+    var to = mapNordstorm.getNodeName( req.body.to )
+    var result = mapNordstorm.getPath( from, to )
+    var stairs;
 
-
-    if (result != null) {
-        res.status(200).json({
+    if ( result != null ) {
+        res.status( 200 ).json( {
             Path: result.path,
+            Stair: stairs,
             TotalCost: result.cost
-        })
+        } )
     } else {
-        res.status(200).json({
+        res.status( 200 ).json( {
             message: "You have arrived !"
-        })
+        } )
     }
 }
 
-exports.getDirection = (req, res, next) => {
-    var current = mapNordstorm.decodeName(req.body.current)
+exports.getDirection = ( req, res, next ) => {
+    var current = mapNordstorm.decodeName( req.body.current )
     var next = req.body.next
+    var stairs;
     var bearing = req.body.bearing
-    var direction = directionController.getDirection(current, next, bearing)
-    console.log(next)
+    var direction = directionController.getDirection( current, next, bearing )
+    console.log( next )
 
-    if (result != null) {
-        res.status(200).json({
-            Navigation: direction
-        })
+    if ( ( current == "H" && next == "H2" ) || ( current == "H2" && next == "H" ) ) {
+        stairs = {
+            containStairs: true,
+            steps: 25
+        }
+
     } else {
-        res.status(200).json({
-            message: "You have arrived !"
-        })
+        stairs = {
+            containStairs: false,
+            steps: 0
+        }
     }
+
+    direction = {
+        ...direction,
+        stairs
+    }
+
+    if ( result != null ) {
+        res.status( 200 ).json( {
+            Navigation: direction
+        } )
+    } else {
+        res.status( 200 ).json( {
+            message: "You have arrived !"
+        } )
+    }
+
+
+
 }
--- directory/map-nordstorm.js
+++ directory/map-nordstorm.js
@@ -1,135 +1,178 @@
-const Graph = require("node-dijkstra");
+const Graph = require( "node-dijkstra" );
 const route = new Graph();
 
 //Initialize all nodes//
-route.addNode("A", {
+route.addNode( "A", {
   B: 13,
   D: 20,
-});
-route.addNode("B", {
+} );
+route.addNode( "B", {
   A: 13,
   C: 8,
   E: 5,
   H: 3
-});
-route.addNode("C", {
+} );
+route.addNode( "C", {
   B: 8,
   E: 4,
   D: 9
-});
-route.addNode("D", {
+} );
+route.addNode( "D", {
   C: 9,
   A: 20,
   F: 12
-});
-route.addNode("E", {
+} );
+route.addNode( "E", {
   B: 5,
   C: 4,
   F: 5,
   G: 6
-});
-route.addNode("F", {
+} );
+route.addNode( "F", {
   G: 12,
   E: 5,
   D: 12
-});
-route.addNode("G", {
+} );
+route.addNode( "G", {
   F: 12,
   E: 6,
   H: 3
-});
-route.addNode("H", {
+} );
+route.addNode( "H", {
   G: 3,
   F: 12,
-  E: 6
-});
+  E: 6,
+  H2: 100,
+} );
+route.addNode( "H2", {
+  I: 5,
+  K: 8
+} );
+route.addNode( "I", {
+  J: 7
+} );
+route.addNode( "J", {
+  K: 7
+} );
 
 //Initialize all sub-nodes//
 let nodeMap = []
-let A = ["Old Navy", "Starbucks", "Nike", "Adidas"]
-let B = ["Samsung", "Bakery", "Tim Hortons", "Pivik", "Bestbuy", "Apple Store"]
-let C = ["Thai Express", "Pizza Pizza", "McDonalds", "Subway"]
-let D = ["Second Cups", "Sport Chek", "H&M", "Roots", "Food Courts"]
+let A = [ "Old Navy", "Starbucks", "Nike", "Adidas" ]
+let B = [ "Samsung", "Bakery", "Tim Hortons", "Pivik", "Bestbuy", "Apple Store" ]
+let C = [ "Thai Express", "Pizza Pizza", "McDonalds", "Subway" ]
+let D = [ "Second Cups", "Sport Chek", "H&M", "Roots", "Food Courts" ]
 let E = []
-let F = ["Bodyshop", "Shawarma Palace", "Nandos", "Popeyes"]
-let G = ["Shoelocker", "EB Games", "Fido", "Freedom"]
+let F = [ "Bodyshop", "Shawarma Palace", "Nandos", "Popeyes" ]
+let G = [ "Shoelocker", "EB Games", "Fido", "Freedom" ]
 let H = []
+let H2 = []
+let I = [ "Photo Studio" ]
+let J = [ "Library" ]
+let K = [ "Paint Shop" ]
 
-A.map(value => {
-  nodeMap.push({
+A.map( value => {
+  nodeMap.push( {
     key: value,
     value: "A"
-  })
-})
+  } )
+} )
 
-B.map(value => {
-  nodeMap.push({
+B.map( value => {
+  nodeMap.push( {
     key: value,
     value: "B"
-  })
-})
+  } )
+} )
 
-C.map(value => {
-  nodeMap.push({
+C.map( value => {
+  nodeMap.push( {
     key: value,
     value: "C"
-  })
-})
+  } )
+} )
 
-D.map(value => {
-  nodeMap.push({
+D.map( value => {
+  nodeMap.push( {
     key: value,
     value: "D"
-  })
-})
+  } )
+} )
 
-E.map(value => {
-  nodeMap.push({
+E.map( value => {
+  nodeMap.push( {
     key: value,
     value: "E"
-  })
-})
+  } )
+} )
 
-F.map(value => {
-  nodeMap.push({
+F.map( value => {
+  nodeMap.push( {
     key: value,
     value: "F"
-  })
-})
+  } )
+} )
 
-G.map(value => {
-  nodeMap.push({
+G.map( value => {
+  nodeMap.push( {
     key: value,
     value: "G"
-  })
-})
+  } )
+} )
 
-H.map(value => {
-  nodeMap.push({
+H.map( value => {
+  nodeMap.push( {
     key: value,
     value: "H"
-  })
-})
+  } )
+} )
+
+H2.map( value => {
+  nodeMap.push( {
+    key: value,
+    value: "H2"
+  } )
+} )
+
+I.map( value => {
+  nodeMap.push( {
+    key: value,
+    value: "I"
+  } )
+} )
+
+J.map( value => {
+  nodeMap.push( {
+    key: value,
+    value: "J"
+  } )
+} )
+
+K.map( value => {
+  nodeMap.push( {
+    key: value,
+    value: "K"
+  } )
+} )
 
 module.exports = {
-  getPath: function (from, to) {
-    return route.path(from, to, {
+  getPath: function ( from, to ) {
+    return route.path( from, to, {
       cost: true
-    });
+    } );
   },
-  getNodeName: function (data) {
+  getNodeName: function ( data ) {
     try {
-      return nodeMap.find(value => value.key === data).value
-    } catch (err) {
+      return nodeMap.find( value => value.key === data ).value
+    } catch ( err ) {
       return false
     }
   },
   test: function () {
     return null
   },
-  decodeName: function (data) {
+  decodeName: function ( data ) {
     var result = ""
-    switch (data) {
+    switch ( data ) {
       case "ADA392FE":
         result = "A"
         break;
@@ -154,6 +197,18 @@ module.exports = {
       case "F3A0A775":
         result = "H"
         break;
+      case "49D74C9A":
+        result = "H2"
+        break;
+      case "43D1BD75": // Need to modify
+        result = "I"
+        break;
+      case "B4B01FA3": // Need to modify
+        result = "J"
+        break;
+      case "F0F875A4": // Need to modify
+        result = "K"
+        break;
     }
     return result
   }
@@ -161,10 +216,15 @@ module.exports = {
 };
 
 
-// A = AD A3 92 FE
-// B = DD A8 38 7E
-// C = ED 8E A9 FE
-// D = BD 20 91 FE
-// E = 8D 97 35 7E
-// F = BD A1 3A 7E
-// G = C1 D1 D9 09
+// A =  AD A3 92 FE
+// B =  DD A8 38 7E
+// C =  ED 8E A9 FE
+// D =  BD 20 91 FE
+// E =  8D 97 35 7E
+// F =  BD A1 3A 7E
+// G =  C1 D1 D9 09
+// H =  F3 A0 A7 75
+// H2 = 49 D7 4C 9A
+// I =  43 D1 BD 75
+// J =  B4 B0 1F A3
+// K =  F0 F8 75 A4
